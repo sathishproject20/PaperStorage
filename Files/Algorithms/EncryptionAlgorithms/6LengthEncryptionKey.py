a2c2cb3d22f0d61ee9f86da9e065846a43fb27c2
Sathish Ramamoorthy <sathishcontact20@gmail.com>
2024-08-04 00:41:17 +0530

    Add files via upload

---
+++ Files/Algorithms/EncryptionAlgorithms/6LengthEncryptionKey.py
@@ -0,0 +1,25 @@
+import itertools
+
+"""
+Total Permutations of 36 Characters in 6 Length String is 2176782336
+
+"""
+
+def generatePermutationsInRange(startIndex, endIndex, numDigits, fileName):
+    assert(numDigits > 0)  # Ensure the number of digits is greater than 0
+    characters = 'ghijklmnopqrstuvwxyz!@#$%^&><_+-*/?='  # Define the custom set of characters
+    count = 0  # Counter to track the number of permutations written
+    with open(fileName, 'w') as fout:  # Open the file in write mode
+        for combination in itertools.product(characters, repeat=numDigits):
+            if count >= startIndex and count <= endIndex:
+                fout.write(''.join(combination) + '\n')  # Write each combination to the file, each on a new line
+            count += 1
+            if count > endIndex:
+                break
+
+if __name__ == "__main__":
+    startIndex = 0  # Start index of permutations to generate
+    endIndex = 999  # End index of permutations to generate (adjust as needed)
+    numDigits = 6  # Number of digits in each permutation
+    fileName = "custom_base_values_36Length_range.txt"  # Output file name
+    generatePermutationsInRange(startIndex, endIndex, numDigits, fileName)
